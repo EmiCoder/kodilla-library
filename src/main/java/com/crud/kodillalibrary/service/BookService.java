365917971b1972ae357340eca55f519d0d1f6d99
EmiCello <luktraczyk@vp.pl>
2020-02-13 12:56:41 +0100

    all requests

--- src/main/java/com/crud/kodillalibrary/service/BookService.java
+++ src/main/java/com/crud/kodillalibrary/service/BookService.java
@@ -1,10 +1,13 @@
 package com.crud.kodillalibrary.service;
 
 import com.crud.kodillalibrary.domain.main.Book;
+import com.crud.kodillalibrary.domain.main.Item;
 import com.crud.kodillalibrary.repository.BookRepository;
+import com.crud.kodillalibrary.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,6 +16,7 @@ public class BookService {
 
     @Autowired
     BookRepository bookRepository;
+
 
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
@@ -34,4 +38,6 @@ public class BookService {
         return bookRepository.findAll().stream()
                 .filter(book -> title.equals(book.getTitle())).collect(Collectors.toList());
     }
+
+
 }
